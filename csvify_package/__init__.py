7f1c0a1ea0d8b113001ac837c843dae11c04698c
PhilipPeters <35991743+PhilipPeters@users.noreply.github.com>
2020-12-11 23:50:45 +0000

    Update __init__.py

--- csvify_package/__init__.py
+++ csvify_package/__init__.py
@@ -1,2 +1,2 @@
-from maincode import comp_csv_write as comp_csv_write
-from maincode import comp_csv_read as comp_csv_read
+from csvify.maincode import comp_csv_write as comp_csv_write
+from csvify.maincode import comp_csv_read as comp_csv_read
